bc9b54ba41a7f94edf384456a712d10f36af3971
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2023-01-31 08:13:53 -0300

    Add files via upload

--- source/Program.cs
+++ source/Program.cs
@@ -1,1 +1,20 @@
+﻿using NexusCRM.Products;
+using NexusCRM.Providers;
 
+Provider fornecedor = new(1, "João Alves");
+fornecedor.SetIdentifier("012.456.789-01");
+fornecedor.SetAddress("Rua zero, numero 0, São Paulo - SP");
+fornecedor.SetPhone("+55 55 9 5555-5555");
+fornecedor.SetEmail("joaoalves@poliloja.com");
+fornecedor.SetCreateDate("31/01/2023 08:06");
+
+Product panela = new(1, "Panela Poliloja SH192D", 90.50m);
+panela.SetDescription("Não gruda ovo");
+panela.SetQuantity(10);
+panela.SetCategory("Cozinha");
+panela.SetCreateDate("31/01/2023 08:06");
+panela.SetTransport("Zedex");
+panela.SetTransportPrice(28.30m);
+panela.SetWarranty("1 Mês contra grudamento de ovos");
+
+panela.AddProvider(fornecedor);
