e49906a22e844d4f6f48d86704cf44d618a99328
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2023-02-01 13:41:14 -0300

    Update Product.cs

--- source/Products/Product.cs
+++ source/Products/Product.cs
@@ -2,10 +2,9 @@ using NexusCRM.Providers;
 
 namespace NexusCRM.Products
 {
-    public class Product
+    public class Product:IComparable<Product>
     {
         public int Id { get; private set; }
-        //private int uid;
         public string Name { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
@@ -16,15 +15,17 @@ namespace NexusCRM.Products
         public string Transport { get; private set; }
         public decimal TransportPrice { get; private set; }
         public string Warranty { get; private set; }
-        public Provider[] Providers { get; private set; }
+        public List<Provider> Providers { get; private set; }
         public Product(int id, string name, decimal price)
         {
             this.Id = id;
             this.Name = name;
             this.Price = price;
             this.Status = 0;
-            this.Providers = new Provider[1];
+            Providers = new List<Provider>();
         }
+        public void SetId(int id) { this.Id = id; }
+        public void SetName(string name) { this.Name = name; }
         public void SetDescription(string text){this.Description = text;}
         public void SetPrice(decimal price){this.Price = price;}
         public void SetQuantity(int quantity){this.Quantity = quantity;}
@@ -36,17 +37,48 @@ namespace NexusCRM.Products
         public void SetWarranty(string warranty) { this.Warranty = warranty;}
         public void SetProvider(Provider provider) 
         { 
-            Providers = new Provider[]{provider};
+            Providers = new List<Provider>{provider};
             provider.AddProductByProvider(this);
         }
-        public void AddProvider(Provider provider) 
+        public void AddProvider(Provider provider2) 
         {
-            Providers.SetValue(provider, Providers[0] == null ? 0 : Providers.Length);
-            provider.AddProductByProvider(this);
+            if(Providers.Count != null)
+            {
+                if (Providers.FirstOrDefault(provider => provider.Id == provider2.Id) != null) { return; }
+            }
+            Providers.Add(provider2);
+            provider2.AddProductByProvider(this);
         }
         public void AddProviderByProduct(Provider provider)
         {
-            Providers.SetValue(provider, Providers[0] == null ? 0 : Providers.Length);
+            Providers.Add(provider);
+        }
+        public int CompareTo(Product other)
+        {
+            if (other == null) return 1;
+            return this.Id.CompareTo(other.Id);
+        }
+        public new string ToString
+        {
+            get
+            {
+                string text = $"ID: {this.Id}" +
+                  $"\nNome: {this.Name}" +
+                  $"\nValor: R$:{this.Price}";
+                if (this.Category != "") { text += $"\nCategoria: {this.Category}"; }
+                if (this.Transport != "") { text += $"\nTipo de transporte: {this.Transport}"; }
+                if (this.TransportPrice != 0m) { text += $"\nValor de transporte: R$:{this.TransportPrice}"; }
+                if (this.Warranty != "") { text += $"\nGarantia: {this.Warranty}"; }
+                if (this.Providers[0] != null)
+                {
+                    text += "\nFornecedores:";
+                    for (int i = 0; i < Providers.Count; i++)
+                    {
+                        text += $"\n[{this.Providers[i].Name}]({this.Providers[i].Id})";
+                    }
+                }
+                return text;
+            }
         }
     }
 }
