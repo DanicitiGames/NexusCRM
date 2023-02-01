875fec31f5f226012afb556edc506b62b8672e84
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2023-02-01 13:42:46 -0300

    Create Client.cs

---
+++ source/Clients/Client.cs
@@ -0,0 +1,45 @@
+namespace NexusCRM.Clients
+{
+    public class Client:IComparable<Client>
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Identifier { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public string Email { get; private set; }
+        public string CreateDate { get; private set; }
+        public int Status { get; private set; }
+        public Client(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Status = 0;
+        }
+        public void SetId(int id) { this.Id = id; }
+        public void SetName(string name) { this.Name = name; }
+        public void SetIdentifier(string identifier) { this.Identifier = identifier; }
+        public void SetAddress(string address) { this.Address = address; }
+        public void SetPhone(string phone) { this.Phone = phone; }
+        public void SetEmail(string email) { this.Email = email; }
+        public void SetCreateDate(string createDate) { this.CreateDate = createDate; }
+        public void SetStatus(int status) { this.Status = status; }
+        public int CompareTo(Client other)
+        {
+            if (other == null) return 1;
+            return this.Id.CompareTo(other.Id);
+        }
+        public new string ToString
+        {
+            get
+            {
+                string text = $"ID: {this.Id}\nNome: {this.Name}";
+                if (this.Identifier != "") { text += $"\nCPF/CNPJ: {this.Identifier}"; }
+                if (this.Address != "") { text += $"\nEndereço: {this.Address}"; }
+                if (this.Phone != "") { text += $"\nTelefone:{this.Phone}"; }
+                if (this.Email != "") { text += $"\nE-mail: {this.Email}"; }
+                return text;
+            }
+        }
+    }
+}
