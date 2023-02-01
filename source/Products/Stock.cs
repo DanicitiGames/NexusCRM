27fdd3e782bbb826058df8deda934ea188f0d290
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2023-02-01 13:41:43 -0300

    Create Stock.cs

---
+++ source/Products/Stock.cs
@@ -0,0 +1,25 @@
+namespace NexusCRM.Products
+{
+    public class Stock:IComparable<Stock>
+    {
+        public Product Product { get; internal set; }
+        public int quantity { get; internal set; }
+        public void SetQuantity(int value)
+        {
+            this.quantity = value;
+        }
+        public void SetProduct(Product product)
+        {
+            this.Product = product;
+        }
+        public string ToString()
+        {
+            return $"Produto: {Product.Name}\nID:{Product.Id}\nQuantidade: {quantity}";
+        }
+        public int CompareTo(Stock other)
+        {
+            if (other == null) return 1;
+            return this.quantity.CompareTo(other.quantity);
+        }
+    }
+}
